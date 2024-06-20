ab279f39941cf2d1fb3cbc6e8bd46eeec2ac9e5a
Ziyan Jiang <ziyjiang@amazon.com>
2024-06-20 06:27:11 -0700

    Clean branch

--- scripts/group_documents.sh
+++
@@ -1,9 +0,0 @@
-python group_documents.py \
-  --processed_wiki_dir "/home/ziyjiang/LongRAG_Data/wiki_2017_abstract" \
-  --mode "abs" \
-  --output_dir "/home/ziyjiang/LongRAG_Data/wiki_2017_abstract_uni"
-
-#python group_documents.py \
-#  --processed_wiki_dir "/home/ziyjiang/LongRAG_Data/wiki" \
-#  --mode "full" \
-#  --output_dir "/home/ziyjiang/LongRAG_Data/wiki"
